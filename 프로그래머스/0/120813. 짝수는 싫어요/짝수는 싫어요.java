1cdcc6283786ec9672a2a78d9cad41558a541e24
hinoky4693 <69946436+hinoky4693@users.noreply.github.com>
2024-06-17 18:32:55 +0900

    Update 짝수는 싫어요.java

--- 프로그래머스/0/120813. 짝수는 싫어요/짝수는 싫어요.java
+++ 프로그래머스/0/120813. 짝수는 싫어요/짝수는 싫어요.java
@@ -1,21 +1,22 @@
-/*
-정수 n이 매개변수로 주어질 때, n 이하의 홀수가 오름차순으로 담긴 배열을 return하도록 solution 함수를 완성해주세요.
-*/
 class Solution {
     public int[] solution(int n) {
         int num = 0;
-        if (n%2 == 0){
-            num = n;
+
+        // 입력받은 n이 짝수인지 확인
+        if (n % 2 == 0) {
+            num = n; // n이 짝수이면 num에 n을 저장
         } else {
-            num = n+1;
+            num = n + 1; // n이 홀수이면 num에 n에 1을 더한 값을 저장
         }
-        int[] answer = new int[num/2];
+
+        // 홀수의 개수는 num의 절반이므로 배열의 크기를 num/2로 설정
+        int[] answer = new int[num / 2];
         
-        for(int i=0;i<(num/2);i++){
-            
-            answer[i] = 2*i+1;
+        // 배열에 홀수 채우기
+        for (int i = 0; i < (num / 2); i++) {
+            answer[i] = 2 * i + 1; // 홀수를 배열에 저장
         }
         
-        return answer;
+        return answer; // 결과 배열 반환
     }
 }
