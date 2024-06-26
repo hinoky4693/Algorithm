a852a690d2cc29f205b7641ee247d3180c82fdef
hinoky4693 <69946436+hinoky4693@users.noreply.github.com>
2024-06-27 08:51:09 +0900

    [level 0] Title: 짝수 홀수 개수, Time: 0.01 ms, Memory: 77.6 MB -BaekjoonHub

---
+++ 프로그래머스/0/120824. 짝수 홀수 개수/짝수 홀수 개수.java
@@ -0,0 +1,16 @@
+class Solution {
+    public int[] solution(int[] num_list) {
+        int[] answer;
+        int odd = 0;
+        int even= 0;
+        
+        for(int i=0;i<num_list.length;i++){
+            if (num_list[i]%2 == 0) even++;
+            else if(num_list[i]%2 == 1) odd++;
+        }
+        
+        answer = new int[] {even, odd};
+        
+        return answer;
+    }
+}
