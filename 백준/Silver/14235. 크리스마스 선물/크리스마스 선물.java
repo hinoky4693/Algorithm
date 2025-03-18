d4165fe7e25880e1dc8cf141e2ceeb35c8498452
hinoky4693 <69946436+hinoky4693@users.noreply.github.com>
2025-03-18 19:44:35 +0900

    [Silver III] Title: 크리스마스 선물, Time: 384 ms, Memory: 35736 KB -BaekjoonHub

---
+++ 백준/Silver/14235. 크리스마스 선물/크리스마스 선물.java
@@ -0,0 +1,27 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.PriorityQueue;
+import java.util.StringTokenizer;
+
+public class Main {
+	public static void main(String[] args) throws Exception {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		PriorityQueue<Integer> pq = new PriorityQueue<>();
+		int n = Integer.parseInt(br.readLine());
+		for(int i=0;i<n;i++) {
+			StringTokenizer st = new StringTokenizer(br.readLine());
+			if (Integer.parseInt(st.nextToken()) == 0) {
+				if(pq.peek() == null) System.out.println(-1);
+				else {
+					System.out.println(-pq.poll());
+				}
+			} else {
+				while(st.hasMoreTokens()) {
+					pq.add(-Integer.parseInt(st.nextToken()));
+				}
+			}
+		}
+		
+		
+	}
+}
