583126be9596a9109080dc5ed081d74b33e9289f
hinoky4693 <69946436+hinoky4693@users.noreply.github.com>
2025-03-23 17:26:49 +0900

    [Silver V] Title: 셀프 넘버, Time: 244 ms, Memory: 82576 KB -BaekjoonHub

--- 백준/Silver/4673. 셀프 넘버/셀프 넘버.java
+++ 백준/Silver/4673. 셀프 넘버/셀프 넘버.java
@@ -7,28 +7,23 @@ public class Main {
 		for(int i=1;i<=10000;i++) {
 			func(i);
 		}
-		
+		StringBuilder sb = new StringBuilder();
 		for(int i=0;i<visited.length;i++) {
-			if(!visited[i]) {
-				System.out.println(i);
-			}
+			if(!visited[i]) sb.append(i).append("\n");
 		}
+		System.out.println(sb);
 	}
 
 	private static void func(int i) {
-		if (i>10000) return;
-		
-		
 		String temp = String.valueOf(i);
 		for(int j=0;j<temp.length();j++) {
 			i += (temp.charAt(j)-'0');
 		}
-		
 		if (i>10000) return;
+		
 		if (!visited[i]) {
 			visited[i] = true;
 		}
-		
 		func(i);
 	}
 }
