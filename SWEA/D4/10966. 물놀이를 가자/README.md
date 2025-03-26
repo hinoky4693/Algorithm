2e3f659af570d82b262c51dc1f8d1e8e75264e06
hinoky4693 <69946436+hinoky4693@users.noreply.github.com>
2025-03-26 20:57:45 +0900

    [D4] Title: 물놀이를 가자, Time: 1,215 ms, Memory: 140,200 KB -BaekjoonHub

---
+++ SWEA/D4/10966. 물놀이를 가자/README.md
@@ -0,0 +1,15 @@
+# [D4] 물놀이를 가자 - 10966 
+
+[문제 링크](https://swexpertacademy.com/main/code/problem/problemDetail.do?contestProbId=AXWXMZta-PsDFAST) 
+
+### 성능 요약
+
+메모리: 140,200 KB, 시간: 1,215 ms, 코드길이: 2,017 Bytes
+
+### 제출 일자
+
+2025-03-26 20:57
+
+
+
+> 출처: SW Expert Academy, https://swexpertacademy.com/main/code/problem/problemList.do
---
+++ SWEA/D4/10966. 물놀이를 가자/물놀이를 가자.java
@@ -0,0 +1,85 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.StringTokenizer;
+
+public class Solution {
+
+	static int T, N, M, sum;
+	static int[][] map;
+	static boolean[][] visited;
+	static int[] dr = { -1, 1, 0, 0 };
+	static int[] dc = { 0, 0, -1, 1 };
+	static Queue<int[]> q = new LinkedList<>();
+
+	public static void main(String[] args) throws Exception {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		T = Integer.parseInt(br.readLine());
+		StringTokenizer st;
+		for (int tc = 1; tc <= T; tc++) {
+			st = new StringTokenizer(br.readLine());
+			N = Integer.parseInt(st.nextToken());
+			M = Integer.parseInt(st.nextToken());
+			sum = 0;
+			map = new int[N][M];
+			for (int r = 0; r < N; r++) {
+				String line = br.readLine();
+				for (int c = 0; c < M; c++) {
+					int data = line.charAt(c) - 'L';
+					if (data != 0)
+						q.add(new int[] { r, c });
+					map[r][c] = line.charAt(c) - 'L';
+
+				}
+			}
+
+			while (!q.isEmpty()) {
+				int[] curr = q.poll();
+				for (int d = 0; d < 4; d++) {
+					int nr = curr[0] + dr[d];
+					int nc = curr[1] + dc[d];
+
+					if (nr >= 0 && nc >= 0 && nr < N && nc < M && map[nr][nc] == 0) {
+						q.add(new int[] { nr, nc });
+						map[nr][nc] = map[curr[0]][curr[1]] + 1;
+					}
+				}
+			}
+			for(int r=0;r<N;r++) {
+				for(int c=0;c<M;c++) {
+					sum += (map[r][c] - 11);
+				}
+			}
+			
+			System.out.println("#" + tc + " " + sum);
+		}
+
+	}
+
+	private static int bfs(int r, int c) {
+		visited[r][c] = true;
+		int count = 0;
+		while (!q.isEmpty()) {
+			int size = q.size();
+			for (int i = 0; i < size; i++) {
+				int[] curr = q.poll();
+				if (map[curr[0]][curr[1]] != 0)
+					return count;
+
+				for (int d = 0; d < 4; d++) {
+					int nr = curr[0] + dr[d];
+					int nc = curr[1] + dc[d];
+
+					if (nr >= 0 && nc >= 0 && nr < N && nc < M && !visited[nr][nc]) {
+						q.add(new int[] { nr, nc });
+						visited[nr][nc] = true;
+					}
+				}
+			}
+			count++;
+		}
+
+		return -1;
+	}
+}
