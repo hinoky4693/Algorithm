a2ee9f2f2b33bf05439febe2f31763868be4a5f5
hinoky4693 <69946436+hinoky4693@users.noreply.github.com>
2025-06-27 15:48:51 +0900

    [level 1] Title: 음양 더하기, Time: 0.07 ms, Memory: 76.4 MB -BaekjoonHub

---
+++ 프로그래머스/1/76501. 음양 더하기/음양 더하기.java
@@ -0,0 +1,11 @@
+class Solution {
+    public int solution(int[] absolutes, boolean[] signs) {
+        int answer = 123456789;
+        int sum = 0;
+        for(int i=0;i<absolutes.length;i++){
+            if(signs[i]) sum += absolutes[i];
+            else sum -= absolutes[i];
+        }
+        return sum;
+    }
+}
