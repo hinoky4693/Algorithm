d909fb6b3a693ddbc0d23229dfdaf2910ddd1c82
hinoky4693 <69946436+hinoky4693@users.noreply.github.com>
2026-01-12 18:56:05 +0900

    [Silver I] Title: 곱셈, Time: 104 ms, Memory: 14300 KB -BaekjoonHub

---
+++ 백준/Silver/1629. 곱셈/곱셈.java
@@ -0,0 +1,16 @@
+import java.util.*;
+import java.math.*;
+import java.io.*;
+
+class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        
+        BigInteger A = new BigInteger(st.nextToken());
+        BigInteger B = new BigInteger(st.nextToken());
+        BigInteger C = new BigInteger(st.nextToken());
+        
+	System.out.println(A.modPow(B, C));
+    }
+}
