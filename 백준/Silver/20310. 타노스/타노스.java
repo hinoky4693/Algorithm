90fb2322e091b6aef435a3bf5d946e9ff246695c
hinoky4693 <69946436+hinoky4693@users.noreply.github.com>
2026-04-14 23:05:42 +0900

    [Silver III] Title: 타노스, Time: 108 ms, Memory: 14268 KB, Score: 100 point -BaekjoonHub

--- 백준/Silver/20310. 타노스/타노스.java
+++ 백준/Silver/20310. 타노스/타노스.java
@@ -5,22 +5,38 @@ public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         
-        String S = br.readLine();
+        char[] S = br.readLine().toCharArray();
         
         int count0 = 0;
+        int count1 = 0;
         
-        for(int i=0; i<S.length(); i++) {
-            if(S.charAt(i) == '0') count0++;    
+        for(int i=0; i<S.length; i++) {
+            if(S[i] == '0') count0++;
+            else count1++;
         }
         
         StringBuilder sb = new StringBuilder();
-        
-        for(int i=0; i<(count0/2); i++) {
-            sb.append('0');
+
+        int count = 0;
+        for(int i=0; i<S.length; i++) {
+            if(S[i] == '1') {
+                S[i] = '.';
+                count++;
+                if(count == count1/2) break;
+            }
+        }
+        count = 0;
+        for(int i=S.length-1; i>=0; i--) {
+            if(S[i] == '0') {
+                S[i] = '.';
+                count++;
+                if(count == count0/2) break;
+            }
         }
         
-        for(int i=0; i<(S.length() - count0)/2; i++) {
-            sb.append('1');
+        for(int i=0; i<S.length; i++) {
+            if(S[i] == '.') continue;
+            sb.append(S[i]);
         }
         
         System.out.println(sb.toString());
