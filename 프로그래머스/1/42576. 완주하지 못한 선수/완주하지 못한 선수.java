90f9c275614b4c04d0ca20be94d145cbf1c05236
hinoky4693 <69946436+hinoky4693@users.noreply.github.com>
2026-06-26 15:29:22 +0900

    [level 1] Title: 완주하지 못한 선수, Time: 186.49 ms, Memory: 115 MB -BaekjoonHub

--- 프로그래머스/1/42576. 완주하지 못한 선수/완주하지 못한 선수.java
+++ 프로그래머스/1/42576. 완주하지 못한 선수/완주하지 못한 선수.java
@@ -1,19 +1,20 @@
-// 1. participant랑 completion 둘다 sort함.
-// 2. participant for문 돌려서 !.equals이면 그 값 return.
-
 import java.util.*;
+
 class Solution {
     public String solution(String[] participant, String[] completion) {
+        String answer = "";
+        
         Arrays.sort(participant);
         Arrays.sort(completion);
-        String answer = "";
-        for(int i=0;i<completion.length;i++){
-            if(!participant[i].equals(completion[i])){
+        answer = participant[participant.length-1];
+        
+        for(int i=0; i<completion.length; i++) {
+            if (!completion[i].equals(participant[i])) {
                 answer = participant[i];
                 break;
             }
         }
-        if (answer.equals("")) answer = participant[participant.length-1];
+        
         return answer;
     }
 }
