26fbe2590dcecb7aad202a28b95f851725f8489c
hinoky4693 <69946436+hinoky4693@users.noreply.github.com>
2026-06-29 16:55:30 +0900

    [level 2] Title: 게임 맵 최단거리, Time: 5.64 ms, Memory: 66 MB -BaekjoonHub

--- 프로그래머스/2/1844. 게임 맵 최단거리/게임 맵 최단거리.java
+++ 프로그래머스/2/1844. 게임 맵 최단거리/게임 맵 최단거리.java
@@ -1,56 +1,55 @@
 import java.util.*;
 
 class Solution {
+    
     int[] dr = {1, 0, -1, 0};
     int[] dc = {0, -1, 0, 1};
-    int[][] map;
-    int mapN;
-    int mapM;
     
+    int mapN, mapM;
+    
+    int[][] maps;
     boolean[][] visited;
-    int answer;
     
     public int solution(int[][] maps) {
-        map = maps;
+        
+        this.maps = maps;
+        
         mapN = maps.length;
         mapM = maps[0].length;
+        
         visited = new boolean[mapN][mapM];
         
-        
-        int answer = 0;
-        answer = bfs(0, 0, 0);
-        return answer;
+        return bfs();
     }
     
-    public int bfs(int r, int c, int dist) {
-        int answer = Integer.MAX_VALUE;
-        visited[r][c] = true;
+    public int bfs() {
         
         Queue<int[]> q = new LinkedList<>();
-        q.add(new int[] {r, c, 1});
+        
+        q.add(new int[] {0, 0, 1});
+        visited[0][0] = true;
+        
         while(!q.isEmpty()) {
             int[] curr = q.poll();
-
-            if(curr[0] == mapN - 1 && curr[1] == mapM - 1) {
-                answer = Math.min(answer, curr[2]);
-            }
             
+            if(curr[0] == mapN - 1 && curr[1] == mapM - 1) return curr[2];
             
             for(int d = 0; d < 4; d++) {
                 int nr = curr[0] + dr[d];
                 int nc = curr[1] + dc[d];
-
+                
                 if(nr >= mapN || nc >= mapM || nr < 0 || nc < 0) continue;
-                if(map[nr][nc] == 0) continue;
                 if(visited[nr][nc]) continue;
-
-
-                q.add(new int[] {nr, nc, curr[2]+1});
+                if(maps[nr][nc] == 0) continue;
+                
+                q.add(new int[] {nr, nc, curr[2] + 1});
                 visited[nr][nc] = true;
             }
         }
         
-        if (answer == Integer.MAX_VALUE) answer = -1;
-        return answer;
+        
+        return -1;
     }
+    
+    
 }
