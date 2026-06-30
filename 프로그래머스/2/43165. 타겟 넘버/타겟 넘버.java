842b7789419ddd6582c8ed608d929eac170bda2d
hinoky4693 <69946436+hinoky4693@users.noreply.github.com>
2026-06-30 15:53:10 +0900

    [level 2] Title: 타겟 넘버, Time: 5.15 ms, Memory: 78.9 MB -BaekjoonHub

---
+++ 프로그래머스/2/43165. 타겟 넘버/타겟 넘버.java
@@ -0,0 +1,30 @@
+class Solution {
+    int[] numbers;
+    int target;
+    int answer;
+    
+    public int solution(int[] numbers, int target) {
+        
+        this.numbers = numbers;
+        this.target  = target;
+        this.answer  = answer;
+        
+        dfs(0, 0);
+        
+        return answer;
+    }
+    
+    public void dfs(int index, int sum) {
+        
+        if(index == numbers.length) {
+            if(sum == target) answer++;
+            return;
+        }
+        
+        
+        dfs(index+1, sum + numbers[index]);
+        dfs(index+1, sum - numbers[index]);
+        
+    }
+    
+}
