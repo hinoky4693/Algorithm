f3bd69dfcae2816a5e28258962ab1edf23abe006
hinoky4693 <69946436+hinoky4693@users.noreply.github.com>
2026-07-08 19:05:13 +0900

    [level 2] Title: 올바른 괄호, Time: 8.64 ms, Memory: 58.1 MB -BaekjoonHub

---
+++ 프로그래머스/2/12909. 올바른 괄호/올바른 괄호.java
@@ -0,0 +1,21 @@
+import java.util.*;
+
+class Solution {
+    boolean solution(String s) {
+        Stack<Character> stack = new Stack<>();
+        
+        char[] arr = s.toCharArray();
+        
+        for(char c : arr) {
+            if(c == '(') stack.push(c);
+            else {
+                if(stack.isEmpty()) return false;
+                stack.pop();
+            }
+        }
+        
+        if(!stack.isEmpty()) return false;
+        
+        return true;
+    }
+}
