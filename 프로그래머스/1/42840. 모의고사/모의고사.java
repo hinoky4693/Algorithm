67fdb1b039a1531ad4a212d0a94071768fe0d343
hinoky4693 <69946436+hinoky4693@users.noreply.github.com>
2026-07-08 18:02:40 +0900

    [level 1] Title: 모의고사, Time: 0.78 ms, Memory: 79.1 MB -BaekjoonHub

--- 프로그래머스/1/42840. 모의고사/모의고사.java
+++ 프로그래머스/1/42840. 모의고사/모의고사.java
@@ -1,38 +1,60 @@
 import java.util.*;
 
-
 class Solution {
     public int[] solution(int[] answers) {
-        int[] arr1 = {1,2,3,4,5};
-        int[] arr2 = {2,1,2,3,2,4,2,5};
-        int[] arr3 = {3,3,1,1,2,2,4,4,5,5};
+        
+        int[] pattern1 = {1, 2, 3, 4, 5};
+        int[] pattern2 = {1, 3, 4, 5};
+        int[] pattern3 = {3, 3, 1, 1, 2, 2, 4, 4, 5, 5};
         
         int count1 = 0;
         int count2 = 0;
         int count3 = 0;
         
-        for(int i=0;i<answers.length;i++) {
-            if (answers[i] == arr1[i%5]) count1++;
-            if (answers[i] == arr2[i%8]) count2++;
-            if (answers[i] == arr3[i%10]) count3++;
+        int index = 0;
+        for(int answer : answers) {
+            if (answer == pattern1[index%5]) count1++;
+            index++;
         }
         
-        int[] arr = {count1, count2, count3};
-        
-        int max = Math.max(Math.max(count1, count2), count3);
-        
-        List<Integer> list = new ArrayList<>();
-        
-        
-        for(int i=0;i<3;i++) {
-            if (arr[i] == max) {
-                list.add(i+1);        
+        index = 0;
+        for(int answer : answers) {
+            if (index % 2 == 0) {
+                if(answer == 2) count2++;
             }
+            else {
+                if (answer == pattern2[(index/2)%4]) count2++;
+            }
+            index++;
         }
         
-        int[] answer = new int[list.size()];
-        for(int i=0;i<answer.length;i++){
-            answer[i] = list.get(i);
+        index = 0;
+        for(int answer : answers) {
+            if (answer == pattern3[index%10]) count3++;
+            index++;
+        }
+        
+        List<Integer> answerList = new ArrayList<>();
+        
+        
+        int max = 0;
+        max = Math.max(Math.max(count1, count2), count3);
+        
+        if (count1 == max) {
+            answerList.add(1);
+        }
+        if (count2 == max) {
+            answerList.add(2);
+        }
+        if (count3 == max) {
+            answerList.add(3);
+        }
+        
+        int[] answer = new int[answerList.size()];
+        
+        index = 0;
+        for(int i : answerList) {
+            answer[index++] = i;
         }
         
         return answer;
